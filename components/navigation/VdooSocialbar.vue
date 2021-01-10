2058147fb7ccc84b2ea6e2ec4c05e1ce8086d84c
mk-ryan1988 <mkryan1988@gmail.com>
2021-01-10 23:06:49 +0000

    Added padding to social bar

--- components/navigation/VdooSocialbar.vue
+++ components/navigation/VdooSocialbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div ref="socialBar" :class="{ 'px-2 py-4 md:mt-12': !inline }" class="flex rounded w-full overflow-hidden md:ml-0 md:mb-0 shadow-2xl md:shadow-none glass lg:bg-transparent lg:dark:bg-transparent">
-      <ul class="flex flex-grow items-center justify-evenly text-xs">
+      <ul class="flex flex-grow items-center justify-evenly text-xs py-2">
         <li class="mx-4 nav-link">
             <a href="https://twitter.com/mkryan1988" rel="noopener" target="_blank" class="flex items-center">
                 <VbaseIcon iconName="twitter" />
