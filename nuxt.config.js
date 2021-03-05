de65b83f00ded4736d2af91a07c71130bff5b739
mk-ryan1988 <mkryan1988@gmail.com>
2021-03-05 00:31:18 +0000

    Updated social img link

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 const title = process.env.npm_package_name || "";
-const image = require('./assets//social-share-img.jpg');
-const description = process.env.npm_package_description || ""
+const image = '/social-share-img.jpg';
+const description = process.env.npm_package_description || "";
 
 export default {
   target: 'static',
@@ -94,16 +94,6 @@ export default {
       short_name: 'mkryan',
       lang: 'en',
       icons: [
-        {
-          "src": "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✌</text></svg>",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✌</text></svg>",
-          "sizes": "512x512",
-          "type": "image/png"
-        },
       ],
     },
   },
